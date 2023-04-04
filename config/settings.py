a86dcb5d2bb779a011eaf2e39070cb951994fcd8
Vadzim <vadzimk@hotmail.com>
2023-04-04 15:54:24 -0700

    add courses app settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'courses',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media'  # base url for media files
+MEDIA_ROOT = BASE_DIR / 'media'  # local path for media files for development
