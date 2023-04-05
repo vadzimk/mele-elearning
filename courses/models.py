41845cece0d1e51e2c98a85c8da4ebadc9ecf0ec
Vadzim <vadzimk@hotmail.com>
2023-04-04 20:31:48 -0700

    create content models hierarchy

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -41,3 +43,42 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField()  # target object id
+    item = GenericForeignKey('content_type', 'object_id')  # target as relationship
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name="%(class)s_related",  # text_related, file_related, video_related etc.
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(
+        upload_to='images')  # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.FileField.upload_to
+
+
+class Video(ItemBase):
+    url = models.URLField()
