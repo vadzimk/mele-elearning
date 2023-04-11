32482516d780347785e0340fe532614bd0391821
Vadzim <vadzimk@hotmail.com>
2023-04-11 02:04:23 -0700

    add nested serializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from courses.models import Subject
+from courses.models import Subject, Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -9,3 +9,19 @@ class SubjectSerializer(serializers.ModelSerializer):
         # specifies fields to be included in json, if not specified, all are included
         fields = ['id', 'title', 'slug']
 
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meat:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    # nested serializer allows to serialize one-many relationship - include fields from the many side
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner',
+                  'modules']
