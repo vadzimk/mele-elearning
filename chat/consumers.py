0e215404b6c35ca70ae9130837ec11ef32b075b8
Vadzim <vadzimk@hotmail.com>
2023-04-13 08:00:14 -0700

    add channels and groups broadcasting with redis

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,6 @@
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -8,15 +9,38 @@ class ChatConsumer(WebsocketConsumer):
     echoes back to the client every message (no broadcasting) """
     def connect(self):
         """ invoked when new connection is received """
-        self.accept()  # accepts any connection
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()  # accepts this connection
 
     def disconnect(self, code):
         """ invoked when client disconnects """
-        pass  # no action needed
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         """ invoked when data are received """
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # send message to client
-        self.send(text_data=json.dumps({'message': message}))
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',  # corresponds to the name of the method that is invoked on consumers that receive the event
+                'message': message,
+            }
+        )
+
+    def chat_message(self, event):
+        """ method name matches type key that is sent to the channel group
+        when a message is received from the websocket """
+        # send message to websocket
+        self.send(text_data=json.dumps(event))
--- config/settings.py
+++ config/settings.py
@@ -12,12 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -30,13 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'daphne',  # need for channels
     'channels',  # intercepts all
-
 
     # local
     'courses',
@@ -52,7 +49,6 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'chat',
 
-
     # default
     'django.contrib.admin',
     'django.contrib.auth',
@@ -62,12 +58,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3d party
-    'redisboard',  # needs to pass its migrations to db after install and in admin panel add redis server redis://localhost:6380/0  where 0 is the database number for redis
+    'redisboard',
+    # needs to pass its migrations to db after install and in admin panel add redis server redis://localhost:6380/0  where 0 is the database number for redis
 
     'rest_framework',  # django rest framework
-
-
-
 
 ]
 # middleware is invoked in the given order during request phase
@@ -87,7 +81,7 @@ MIDDLEWARE = [
 
 # per site cache middleware will cache all views including the admin dashboard (not good)
 CACHE_MIDDLEWARE_ALIAS = 'default'
-CACHE_MIDDLEWARE_SECONDS = 60*15  # fifteen minutes
+CACHE_MIDDLEWARE_SECONDS = 60 * 15  # fifteen minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'  # project prefix in case many projects use the same backend
 
 ROOT_URLCONF = 'config.urls'
@@ -108,11 +102,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application' # replaced by asgi
+WSGI_APPLICATION = 'config.wsgi.application'  # replaced by asgi
 
 ASGI_APPLICATION = 'config.asgi.application'  # for the channels 3d party app
-
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -123,7 +115,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -143,7 +134,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -154,7 +144,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -168,7 +157,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'  # base url for media files
 MEDIA_ROOT = BASE_DIR / 'media'  # local path for media files for development
-
 
 CACHES = {
     # 'default': {
@@ -192,3 +180,13 @@ REST_FRAMEWORK = {
     ]
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [
+                (os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT')),
+            ]
+        }
+    }
+}
